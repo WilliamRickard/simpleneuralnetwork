17bcc011438b5bb655873ad5441fb60213444099
WilliamRickard <wttr2597@gmail.com>
2026-09-15 19:00:39 +0100

    Propagate no-main guard through v20

--- v20/main.cpp
+++ v20/main.cpp
@@ -3,9 +3,9 @@
 #pragma GCC diagnostic ignored "-Wmisleading-indentation"
 #pragma GCC diagnostic ignored "-Wunused-function"
 #endif
-#define main v19_embedded_main
+#define SIMPLE_NN_V19_NO_MAIN
 #include "../v19/main.cpp"
-#undef main
+#undef SIMPLE_NN_V19_NO_MAIN
 #if defined(__GNUC__)
 #pragma GCC diagnostic pop
 #endif
@@ -208,6 +208,7 @@ static void offlineRunV20(size_t rowCount,const OptimiserConfig&optimiser,double
     if(!result.reachedTarget)cout<<"Offline L-BFGS reached the maximum number of iterations before the target.\n";
 }
 
+#ifndef SIMPLE_NN_V20_NO_MAIN
 int main(){
     try{
         bool batchOnline=false,offline=false,test=true,randomiseWeights=false;
@@ -243,3 +244,4 @@ int main(){
         return 1;
     }
 }
+#endif
